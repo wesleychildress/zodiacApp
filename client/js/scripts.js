72e92afa86607171cbbbd1176fef3ccb924521e9
WesleyChildress <wesleychildress@icloud.com>
2020-02-15 22:18:05 -0500

    update index.js

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,3 +1,17 @@
+function entFunc(){
+    $("#searchForm").submit(function(e) {
+        e.preventDefault();
+        getData();
+        return false;
+    });
+    
+    $('#myBtn').click(function(e){
+        e.preventDefault();
+        // call function
+        getData();
+    });
+}
+
 var zodiacs = [
 {
 	name: "Aries",
